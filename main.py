4a4a0bfe9206cc8329c7c558630e6343ee6a914b
KrisNathan <kristophernathanael2005@gmail.com>
2023-06-23 16:21:43 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import re
+import os
+
+os.system(f'wget https://nscpolteksby.ac.id/ebook/book/computer-engineering/')
+
+with open("index.html", "r") as f:
+    title = ""
+    for line in f:
+        title_search = re.search(r'(?<=href=\"#\">).*(?=<b>)', line)
+        pdf_search = re.search(r"(?<=href=\")files\/Ebook\/.*\.pdf(?=\")", line)
+        if title_search:
+            sstring = title_search.group()
+            print(sstring)
+            title = sstring
+
+        if pdf_search:
+            sstring_p = pdf_search.group()
+            url = re.sub(r'files\/', 'https://nscpolteksby.ac.id/ebook/files/', sstring_p)
+            print(url)
+            os.system(f'wget -P "files/{title}" "{url}"')
